57a71127a1ddaac17c4b7aa2a8a2b858a045c0d1
MathMark <vadimmartsun@gmail.com>
2020-01-30 22:26:38 +0200

    Initial commit

---
+++ src/main/java/com/hillel/mvc/springboot/model/User.java
@@ -0,0 +1,18 @@
+package com.hillel.mvc.springboot.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private int id;
+    private String lastName;
+    private String firstName;
+    private LocalDate birthDate;
+    private Gender gender;
+}
